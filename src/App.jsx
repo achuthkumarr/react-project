21ef2d69378d0d1b6e738a15d897cc140d08cd88
Achuth Kumar R <achuthkumarr@Achuths-MacBook-Air.local>
2025-03-06 17:36:14 +0530

    home name is change to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Home from "./Components/home";
 import About from "./Components/About";
 import Contact from "./Components/Content";
+import Site from "./Components/Site";
+
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <><Navbar/><Home /></> },
+    { path: "/", element: <><Navbar/><Site /></> },
     { path: "/About", element: <><Navbar/><About /></> },
     { path: "/Contact", element: <><Navbar/><Contact /></> },
   ]);
