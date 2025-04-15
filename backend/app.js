671720fb5f4e70885b6ee1a6bdcd43cc34b853ee
Achuth kumar r <acuthkumar11@gmail.com>
2025-04-15 10:47:59 +0530

    normal commit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Define CORS options
 const corsOptions = {
-  origin: ['http://localhost:5174','http://localhost:5173'],
+  origin: ['http://localhost:5174'],
 };
 
 // Middleware
